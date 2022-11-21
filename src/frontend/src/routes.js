0f522206f811949a31285e7af54fa4e85657033c
Veronika Machova <mach.veronika@gmail.com>
2022-11-21 13:34:39 +0100

    Vue registration template added

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router"
 import UserLogin from "@/views/UserLogin"
 import HomePage from "@/views/HomePage"
 import UserProfile from "@/views/UserProfile"
+import UserRegistration from "@/views/UserRegistration"
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ export default new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
+        {
+            path: '/register',
+            name: 'register',
+            component: UserRegistration
+        },
         {
             path: '/login',
             name: 'login',
--- src/main/java/com/sema/kittinder/controllers/AuthController.java
+++ src/main/java/com/sema/kittinder/controllers/AuthController.java
@@ -3,10 +3,8 @@ package com.sema.kittinder.controllers;
 import com.sema.kittinder.services.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
