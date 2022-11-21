217d0b1e385e1e75b2bacd9fb83171afa4d4fa7a
Veronika Machova <mach.veronika@gmail.com>
2022-11-21 16:02:32 +0100

    idle Time added, logout added, getters in Vuex added

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import store from './store'
+import IdleVue from 'idle-vue'
 
 Vue.config.productionTip = false
-
+const eventsHub = new Vue()
+Vue.use(IdleVue, {
+  eventEmitter: eventsHub,
+  idleTime: 1800000
+})
+router.beforeEach((to, from, next) => {
+  if (to.matched.some( record => record.meta.requiresLogin)) {
+    if (!store.getters.loggedIn) {
+      next({ name: 'login'})
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
 new Vue({
   router,
   store,
---
+++ src/frontend/src/views/UserLogout.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h1>Bye bye</h1>
+  <button><router-link to="/login">Beck to login?</router-link></button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UserLogout",
+  created(){
+    this.$store.dispatch('userLogout')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
