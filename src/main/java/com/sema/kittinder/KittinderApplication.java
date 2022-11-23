767b95adcfc0810e9d13ee3bc3935dc9d5547e2d
domsebesta <dominik.sebesta@outlook.com>
2022-11-23 11:57:40 +0100

    frontend mecheche

--- src/main/java/com/sema/kittinder/KittinderApplication.java
+++ src/main/java/com/sema/kittinder/KittinderApplication.java
@@ -3,11 +3,10 @@ package com.sema.kittinder;
 import com.sema.kittinder.configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class KittinderApplication {
 
   public static void main(String[] args) {
